f88c5c109c91425338b0199ef8cc5603343d82fb
Homero Pawlowski <homeski2@gmail.com>
2017-07-19 16:13:34 -0400

    rsync now excludes anything in the top level .gitignore file

--- remote-sync.sh
+++ remote-sync.sh
@@ -1,3 +1,3 @@
 #!/bin/bash
 
-fswatch .. | xargs -t -n 1 -I {} rsync --verbose --delete --exclude-from='remote-sync-exclude' --archive .. `cat ./remote-sync.conf`
+fswatch .. | xargs -t -n 1 -I {} rsync --verbose --delete --exclude-from='../.gitignore' --exclude-from='remote-sync-exclude' --archive .. `cat ./remote-sync.conf`
